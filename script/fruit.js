afb5e769dac29264f7840e63c49ff1867ba6007d
AGB004 <alecbyington@gmail.com>
2022-12-14 18:12:50 -0700

    update

--- script/fruit.js
+++ script/fruit.js
@@ -4,60 +4,36 @@ fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
   .then(function (response) {
     return response.json();
   })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const fruits = jsonObject['fruits'];
+  .then(function (data) {
+    console.table(data);  // temporary checking for valid response and data parsing
+    const fruits = data['fruits'];
     fruits.forEach(displayFruits);
   });
 
         // working on!!!!!
 
 function displayFruits(fruit) {
-  let card = document.createElement('section');
-  let image = document.createElement('img');
+  let card = document.createElement('div');
   let h2 = document.createElement('h2');
-  let address = document.createElement('p');
-  let phoneNumber = document.createElement('p');
-  let website = document.createElement('a');
+  let nutri = document.createElement('ul');
+  let carbs = document.createElement('li');
+  let pro = document.createElement('li');
+  let cal = document.createElement('li');
+  let sug = document.createElement('li');
 
-  h2.textContent = business.name;
-  address.textContent = `Address: ${business.address}`;
-  phoneNumber.textContent = `Contact: ${business.phoneNumber}`;
-  website.textContent = business.webURL;
-  website.href = business.webURL;
-
-  image.setAttribute('src', business.image);
-  image.setAttribute('alt', `Image of ${business.name}`);
-  image.setAttribute('class', "directoryImg");
-  image.setAttribute('loading', 'lazy');
+  h2.textContent = fruit.name;
+  carbs.textContent = fruit.carbohydrates;
+  pro.textContent = fruit.protein;
+  cal.textContent = fruit.calories;
+  sug.textContent = fruit.sugar;
 
   card.appendChild(h2);
-  card.appendChild(image);
-  card.appendChild(address);
-  card.appendChild(phoneNumber);
-  card.appendChild(website);
+  nutri.appendChild(carbs);
+  nutri.appendChild(pro);
+  nutri.appendChild(cal);
+  nutri.appendChild(sug);
+  card.appendChild(nutri);
 
-  const grid = document.querySelector('.directoryGrid');
-  grid.appendChild(card);
-
-  
-}
-
-// Grid & List Layout Buttons
-const gridButton = document.querySelector('#grid');
-const listButton = document.querySelector('#list');
-const arrange = document.querySelector('#businesses');
-
-if (gridButton) {
-  gridButton.addEventListener('click', () => {
-    arrange.classList.add('directoryGrid');
-    arrange.classList.remove('directoryList');
-  });
-}
-
-if (listButton) {
-  listButton.addEventListener('click', () => {
-    arrange.classList.add('directoryList');
-    arrange.classList.remove('directoryGrid');
-  });
+  const display = document.querySelector('#fruits');
+  display.appendChild(card);
 }
