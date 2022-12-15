055cca3e0790d110a41c602d6d0abc2c651923f7
AGB004 <alecbyington@gmail.com>
2022-12-14 18:26:29 -0700

    update

--- script/fruit.js
+++ script/fruit.js
@@ -6,33 +6,57 @@ fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const fruits = jsonObject['fruits'];
-    fruits.forEach(displayFruits);
+    fruits.forEach(function (fruit) {
+        let card = document.createElement('div');
+        let h2 = document.createElement('h2');
+        let nutri = document.createElement('ul');
+        let carbs = document.createElement('li');
+        let pro = document.createElement('li');
+        let cal = document.createElement('li');
+        let sug = document.createElement('li');
+      
+        h2.textContent = fruit.name;
+        carbs.textContent = fruit.carbohydrates;
+        pro.textContent = fruit.protein;
+        cal.textContent = fruit.calories;
+        sug.textContent = fruit.sugar;
+      
+        card.appendChild(h2);
+        nutri.appendChild(carbs);
+        nutri.appendChild(pro);
+        nutri.appendChild(cal);
+        nutri.appendChild(sug);
+        card.appendChild(nutri);
+      
+        const display = document.querySelector('#fruits');
+        display.appendChild(card);
+      });
   });
 
         // working on!!!!!
 
-function displayFruits(fruit) {
-  let card = document.createElement('div');
-  let h2 = document.createElement('h2');
-  let nutri = document.createElement('ul');
-  let carbs = document.createElement('li');
-  let pro = document.createElement('li');
-  let cal = document.createElement('li');
-  let sug = document.createElement('li');
+// function displayFruits(fruit) {
+//   let card = document.createElement('div');
+//   let h2 = document.createElement('h2');
+//   let nutri = document.createElement('ul');
+//   let carbs = document.createElement('li');
+//   let pro = document.createElement('li');
+//   let cal = document.createElement('li');
+//   let sug = document.createElement('li');
 
-  h2.textContent = fruit.name;
-  carbs.textContent = fruit.carbohydrates;
-  pro.textContent = fruit.protein;
-  cal.textContent = fruit.calories;
-  sug.textContent = fruit.sugar;
+//   h2.textContent = fruit.name;
+//   carbs.textContent = fruit.carbohydrates;
+//   pro.textContent = fruit.protein;
+//   cal.textContent = fruit.calories;
+//   sug.textContent = fruit.sugar;
 
-  card.appendChild(h2);
-  nutri.appendChild(carbs);
-  nutri.appendChild(pro);
-  nutri.appendChild(cal);
-  nutri.appendChild(sug);
-  card.appendChild(nutri);
+//   card.appendChild(h2);
+//   nutri.appendChild(carbs);
+//   nutri.appendChild(pro);
+//   nutri.appendChild(cal);
+//   nutri.appendChild(sug);
+//   card.appendChild(nutri);
 
-  const display = document.querySelector('#fruits');
-  display.appendChild(card);
-}
+//   const display = document.querySelector('#fruits');
+//   display.appendChild(card);
+// }
